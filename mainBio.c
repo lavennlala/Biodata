9dc0e8e790a75966d2c165db002530e208901231
lavennlala <laveniardana9@gmail.com>
2020-09-23 11:04:46 +0700

    Biodata

---
+++ mainBio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Biodata \n\n");
+	printf("Nama Lengkap			: Lavenia Aglis Ardana \n");
+	printf("Nama Panggilan			: Lavenia \n");
+	printf("Tempat, Tanggal Lahir		: Surabaya, 9 Mei 2002 \n");
+	printf("Alamat Rumah			: Perum. Permata Green Menganti Regency D1.50 \n");
+	printf("Status				: Mahasiswa \n");
+	printf("Jurusan				: Teknik Informatika \n");
+	printf("Universitas			: UPN Veteran Jawa Timur \n");
+	
+	return 0;
+}
